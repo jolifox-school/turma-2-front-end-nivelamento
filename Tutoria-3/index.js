a87a0617bbeced3222c62a89797d8ab4ce9f89ff
Rafael <rafaelbrunodasilva@yahoo.com.br>
2023-03-22 20:06:00 -0300

    T2-32-desafio-front-end_final

--- Tutoria-3/index.js
+++ Tutoria-3/index.js
@@ -1,2 +1,15 @@
 "use strict";
 
+const menuItems = document.querySelectorAll(".menu-items li");
+
+menuItems.forEach((e) => {
+    console.log(e.innerHTML);
+    e.addEventListener("click", toggleActive, false);
+});
+
+function toggleActive() {
+    menuItems.forEach((e) => {
+        e.classList.remove("active");
+    });
+    this.classList.add("active");
+}
