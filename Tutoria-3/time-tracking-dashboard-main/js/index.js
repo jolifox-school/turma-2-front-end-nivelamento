711abd983461ae60603cc282df2591ea976cdf84
Janderson-Weller <janderson.weller@gmail.com>
2023-03-27 21:20:02 -0400

    Cards adicionados de forma dinamica

--- Tutoria-3/time-tracking-dashboard-main/js/index.js
+++ Tutoria-3/time-tracking-dashboard-main/js/index.js
@@ -3,7 +3,7 @@
 *       cada categoria.
 *  2 - Adicionar o evento "click" no menu, onde ao clicar as informações nos cards mudem de acordo com 
 *       o período escolhido
-*/      
+*/
 
 const valores = [
     {
@@ -94,3 +94,53 @@ const valores = [
         ]
     },
 ]
+
+const list = ["Daily", "Weekly", "Monthly"];
+
+
+const createList = () => {
+    list.forEach(item => {
+        const ul = document.querySelector(".ul-lista");
+        const itemDaLista = document.createElement("li");
+        itemDaLista.classList.add("item-lista");
+        itemDaLista.textContent = item;
+        ul.appendChild(itemDaLista)
+    });
+}
+
+createList();
+
+const cardsSuperiores = ["Work", "Play", "Study"];
+const cardInferiores = ["Exercise", "Social", "Self-Care"];
+const superior = document.querySelector(".cards-superiores");
+const inferior = document.querySelector(".cards-inferiores");
+
+const createCard = (arrayCard, elementoPai) => {
+
+    for(i = 0; i < arrayCard.length; i++) {
+        const card = `
+            <div class="card card-${arrayCard[i]}">
+                <header class="header-card">
+                    <span class="titulo-card">${arrayCard[i]}</span>
+                    <span class="opcao-card">°°°</span>
+                </header>
+                <div class="body-card">
+                    <span class="hora-inicio">32hrs</span>
+                </div>
+                <footer class="rodape-card">
+                    <span class="resumo-atividade">Last Week - 36hrs</span>
+                </footer>
+            </div>
+        `
+        const containerCard = document.createElement("div");
+        containerCard.classList.add("container-card");
+        containerCard.classList.add(`container-card-${arrayCard[i]}`);
+        containerCard.innerHTML = card;
+        elementoPai.appendChild(containerCard);
+    }
+}
+
+createCard(cardsSuperiores, superior);
+createCard(cardInferiores, inferior);
+
+
