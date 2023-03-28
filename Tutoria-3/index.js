c4389733c706078a2c4a35b28e84fc1f015c4c9b
Natalia Moraes <naty_amoraes@hotmail.com>
2023-03-28 20:10:24 -0300

    index js added

--- Tutoria-3/index.js
+++ Tutoria-3/index.js
@@ -1,1 +1,34 @@
-console.log("teste")
+const valores = [
+    {
+        "semana": {
+            "work": 14,
+            "play": 10,
+            "study": 12,
+            "exercise": 8,
+            "social": 15,
+            "selfcare": 6,
+        }
+    },
+    {
+        "quinzenal": {
+            "work": 36,
+            "play": 18,
+            "study": 24,
+            "exercise": 20,
+            "social": 15,
+            "selfcare": 12,
+        }
+    },
+    {
+        "mensal": {
+            "work": 42,
+            "play": 24,
+            "study": 36,
+            "exercise": 32,
+            "social": 30,
+            "selfcare": 30,
+        }
+    }
+]
+
+valores.forEach((item) => console.log(item))
