c24f20ad704dfe252580f6162a403204b5f3f3f6
Douglas Hansen <hansenndouglas@gmail.com>
2023-03-29 21:47:33 -0300

    Finalizado JS

--- Tutoria-3/time-tracking-dashboard-main/index.js
+++ Tutoria-3/time-tracking-dashboard-main/index.js
@@ -8,88 +8,88 @@
 
 const valores = [
   {
-    periodo: "daily",
+    periodo: "daily", // valores[0].periodo
     card: [
       {
-        titulo: "work",
+        titulo: "Work",
         valor: 4,
       },
       {
-        titulo: "play",
+        titulo: "Play",
         valor: 6,
       },
       {
-        titulo: "study",
+        titulo: "Study",
         valor: 8,
       },
       {
-        titulo: "exercise",
+        titulo: "Exercise",
         valor: 6,
       },
       {
-        titulo: "social",
+        titulo: "Social",
         valor: 2,
       },
       {
-        titulo: "selfCare",
+        titulo: "Self Care",
         valor: 7,
       },
     ],
   },
   {
-    periodo: "weekly",
+    periodo: "weekly", // valores[1].periodo
     card: [
       {
-        titulo: "work",
+        titulo: "Work",
         valor: 3,
       },
       {
-        titulo: "play",
+        titulo: "Play",
         valor: 6,
       },
       {
-        titulo: "study",
+        titulo: "Study",
         valor: 8,
       },
       {
-        titulo: "exercise",
+        titulo: "Exercise",
         valor: 2,
       },
       {
-        titulo: "social",
+        titulo: "Social",
         valor: 8,
       },
       {
-        titulo: "selfCare",
+        titulo: "Self Care",
         valor: 7,
       },
     ],
   },
   {
-    periodo: "monthly",
+    periodo: "monthly", // valores[2].periodo
     card: [
       {
-        titulo: "work",
+        titulo: "Work",
         valor: 4,
       },
       {
-        titulo: "play",
+        titulo: "Play",
         valor: 4,
       },
       {
-        titulo: "study",
+        titulo: "Study",
         valor: 8,
       },
       {
-        titulo: "exercise",
+        titulo: "Exercise",
         valor: 6,
       },
       {
-        titulo: "social",
+        titulo: "Social",
         valor: 2,
       },
       {
-        titulo: "selfCare",
+        titulo: "Self Care",
         valor: 7,
       },
     ],
@@ -103,34 +103,53 @@ function changeState(toAdd, toRemove1, toRemove2) {
   document.getElementById(toRemove2).classList.remove("selected");
 }
 
-// ------ Melhorar aqui conforme arquivo da tutoria
-// const lista = document.querySelectorAll(".item-lista");
+// Update the hour values
+function updateCards(period) {
+  // Seleciona todas as divs com classe "titulo-card" e "hora-inicio"
+  const tituloCards = document.querySelectorAll(".titulo-card");
+  const horaInicioDivs = document.querySelectorAll(".hora-inicio");
 
-// lista.forEach(itemAlvo => {
-//     itemAlvo.
-// })
-
-// Change selected period
-function changeHours(tag, toRemove1, toRemove2) {
-  document.getElementById(toAdd).classList.add("selected");
-  document.getElementById(toRemove1).classList.remove("selected");
-  document.getElementById(toRemove2).classList.remove("selected");
+  // percorre todas as divs "titulo-card"
+  for (let i = 0; i < tituloCards.length; i++) {
+    // percorre todos os valores/indices principais do array "valores"
+    for (let j = 0; j < valores.length; j++) {
+      // Verifica o periodo selecionado
+      if (valores[j].periodo === period) {
+        // compara o texto da div "titulo-card" com valores[j].card[k].titulo
+        for (let k = 0; k < valores[j].card.length; k++) {
+          if (tituloCards[i].innerText === valores[j].card[k].titulo) {
+            // preenche o texto da div "hora-inicio" correspondente
+            horaInicioDivs[i].innerText = valores[j].card[k].valor + "hrs";
+          }
+        }
+      }
+    }
+  }
 }
+
+// At first moment loading the screen
+window.addEventListener("load", function () {
+  changeState("daily", "weekly", "monthly");
+  updateCards("daily");
+});
 
 // When "Daily" button is pressed
 document.querySelector("#daily").addEventListener("click", function () {
   // Add the class "selected" in daily and removes from the other
   changeState("daily", "weekly", "monthly");
+  updateCards("daily");
 });
 
 // When "weekly" button is pressed
 document.querySelector("#weekly").addEventListener("click", function () {
   // Add the class "selected" in weekly and removes from the other
   changeState("weekly", "daily", "monthly");
+  updateCards("weekly");
 });
 
 // When "monthly" button is pressed
 document.querySelector("#monthly").addEventListener("click", function () {
   // Add the class "selected" in monthly and removes from the other
   changeState("monthly", "daily", "weekly");
+  updateCards("monthly");
 });
