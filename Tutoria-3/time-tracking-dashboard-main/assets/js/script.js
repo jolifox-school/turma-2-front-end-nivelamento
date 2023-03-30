d60ced6583c0fc4b438261c3b2786e48da6948f5
Tomé Luan <tomeluan05@gmail.com>
2023-03-29 22:06:24 -0300

    finalizando o projeto mostrando os dados dinamicamente dependendo de qual botão for clicado

--- Tutoria-3/time-tracking-dashboard-main/assets/js/script.js
+++ Tutoria-3/time-tracking-dashboard-main/assets/js/script.js
@@ -3,13 +3,48 @@
 const content = document.querySelector('.content')
 
 let conteudos_cards = [
-    {titulo: "Work", paragrafo: "Last Week - 36hrs", subtitulo: "32hrs"},
-    {titulo: "Play", paragrafo: "Last Week - 8hrs", subtitulo: "10hrs"},
-    {titulo: "Study", paragrafo: "Last Week - 7hrs", subtitulo: "4hrs"},
-    {titulo: "Exercise", paragrafo: "Last Week - 5hrs", subtitulo: "4hrs"},
-    {titulo: "Social", paragrafo: "Last Week - 10hrs", subtitulo: "5hrs"},
-    {titulo: "Self Care", paragrafo: "Last Week - 2hrs", subtitulo: "2hrs"},
+    {titulo: "Work", paragrafo: "Last Week - 36hrs", subtitulo: 32},
+    {titulo: "Play", paragrafo: "Last Week - 8hrs", subtitulo: 10},
+    {titulo: "Study", paragrafo: "Last Week - 7hrs", subtitulo: 4},
+    {titulo: "Exercise", paragrafo: "Last Week - 5hrs", subtitulo: 4},
+    {titulo: "Social", paragrafo: "Last Week - 10hrs", subtitulo: 5},
+    {titulo: "Self Care", paragrafo: "Last Week - 2hrs", subtitulo: 2},
+]   
+
+const valores = [
+    {
+        "periodo": "daily",
+        "card": [{"titulo": "work", "valor": 4},
+                {"titulo": "play", "valor": 6},
+                {"titulo": "study", "valor": 5},
+                {"titulo": "exercise", "valor": 7},
+                {"titulo": "social", "valor": 3},
+                {"titulo": "selfCare", "valor": 17}
+        ]
+    },
+    {
+        "periodo": "weekly",
+        "card": [
+                {"titulo": "work", "valor": 3}, 
+                {"titulo": "play", "valor": 9},
+                {"titulo": "study","valor": 11},
+                {"titulo": "exercise", "valor": 2},
+                {"titulo": "social", "valor": 8},
+                {"titulo": "selfCare", "valor": 4}
+        ]
+    },
+    {
+        "periodo": "monthly",
+        "card": [{"titulo": "work", "valor": 12},
+                {"titulo": "play", "valor": 4},
+                {"titulo": "study", "valor": 8},
+                {"titulo": "exercise", "valor": 6},
+                {"titulo": "social", "valor": 2},
+                {"titulo": "selfCare", "valor": 7}
+        ]
+    },
 ]
+
 
 conteudos_cards.forEach((conteudo, index)=>{
     content.innerHTML += `
@@ -21,7 +56,7 @@ conteudos_cards.forEach((conteudo, index)=>{
                 <a href="#"><img src="./assets/images/icon-ellipsis.svg" alt=""></a>
             </section>
             <section class="dados_content">
-                <h3>${conteudo.subtitulo}</h3>
+                <h3>${conteudo.subtitulo}hrs</h3>
                 <p>${conteudo.paragrafo}</p>
             </section>
         </section>
@@ -29,7 +64,37 @@ conteudos_cards.forEach((conteudo, index)=>{
     `
 })
 
-    
+const links = document.querySelectorAll('.link')
+const subtitulo = document.querySelectorAll('h3')
 
-
-
+links.forEach((link)=>{
+    link.addEventListener('click',()=>{
+        if(link.innerHTML === 'Daily'){
+            valores.forEach((valor)=>{
+                if(valor.periodo === link.innerHTML.toLowerCase()){
+                    valor.card.forEach((item, index)=>{
+                        subtitulo[index].innerHTML = `${item.valor}hrs`
+                    })
+                }
+            })
+        }
+        else if(link.innerHTML === 'Weekly'){
+            valores.forEach((valor)=>{
+                if(valor.periodo === link.innerHTML.toLowerCase()){
+                    valor.card.forEach((item, index)=>{
+                        subtitulo[index].innerHTML = `${item.valor}hrs`
+                    })
+                }
+            })
+        }
+        else if(link.innerHTML === 'Monthly'){
+            valores.forEach((valor)=>{
+                if(valor.periodo === link.innerHTML.toLowerCase()){
+                    valor.card.forEach((item, index)=>{
+                        subtitulo[index].innerHTML = `${item.valor}hrs`
+                    })
+                }
+            })
+        }
+    })
+})
