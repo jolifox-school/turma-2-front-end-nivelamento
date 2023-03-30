2d40ebcbd77a3d45c6e56f1d168e1d3be115fae6
Natalia Moraes <naty_amoraes@hotmail.com>
2023-03-30 18:32:24 -0300

    Added configuration into index.js - change hour according selection menu

--- Tutoria-3/index.js
+++ Tutoria-3/index.js
@@ -1,36 +1,135 @@
 const valores = [
     {
-        "semana": {
-            "work": 14,
-            "play": 10,
-            "study": 12,
-            "exercise": 8,
-            "social": 15,
-            "selfcare": 6
-        }
+        "periodo": "daily",
+        "card": [
+            {
+                "titulo": "work",
+                "valor": '4hrs'
+            },
+            {
+                "titulo": "play",
+                "valor": '6hrs'
+            },
+            {
+                "titulo": "study",
+                "valor": '8hrs'
+            },
+            {
+                "titulo": "exercise",
+                "valor": '6hrs'
+            },
+            {
+                "titulo": "social",
+                "valor": '2hrs'
+            },
+            {
+                "titulo": "selfCare",
+                "valor": '9hrs'
+            }
+        ]
     },
     {
-        "quinzenal": {
-            "work": 36,
-            "play": 18,
-            "study": 24,
-            "exercise": 20,
-            "social": 15,
-            "selfcare": 12
-        }
+        "periodo": "weekly",
+        "card": [
+            {
+                "titulo": "work",
+                "valor": '13hrs'
+            },
+            {
+                "titulo": "play",
+                "valor": '16hrs'
+            },
+            {
+                "titulo": "study",
+                "valor": '18hrs'
+            },
+            {
+                "titulo": "exercise",
+                "valor": '12hrs'
+            },
+            {
+                "titulo": "social",
+                "valor": '18hrs'
+            },
+            {
+                "titulo": "selfCare",
+                "valor": '17hrs'
+            }
+        ]
     },
     {
-        "mensal": {
-            "work": 42,
-            "play": 24,
-            "study": 36,
-            "exercise": 32,
-            "social": 30,
-            "selfcare": 30
-        }
-    }
+        "periodo": "monthly",
+        "card": [
+            {
+                "titulo": "work",
+                "valor": '34hrs'
+            },
+            {
+                "titulo": "play",
+                "valor": '30hrs'
+            },
+            {
+                "titulo": "study",
+                "valor": '38hrs'
+            },
+            {
+                "titulo": "exercise",
+                "valor": '36hrs'
+            },
+            {
+                "titulo": "social",
+                "valor": '32hrs'
+            },
+            {
+                "titulo": "selfCare",
+                "valor": '27hrs'
+            }
+        ]
+    },
 ]
 
-valores.forEach((item, index) => {
-    console.log(valores[index])
+
+const cursor = document.querySelectorAll('.item-lista')
+cursor.forEach(item => {
+    item.addEventListener('mouseover',() => {
+        item.style.cursor = 'pointer';
 })
+})
+
+//console.log(valores[0].card[0].valor)
+
+const funcaoClick = () => {
+    const lista = document.querySelectorAll(".item-lista");
+    lista.forEach(itemAlvo => {
+        itemAlvo.addEventListener("click", () => {
+            let clicouEm = itemAlvo.textContent
+
+            if(clicouEm == "Daily"){
+                let horaInicio = document.querySelectorAll('.hora-inicio')
+                for(let i = 0; i < valores[0].card.length; i++ ){
+                    let novoValor = valores[0].card[i].valor
+                    horaInicio[i].textContent = novoValor
+                    }
+            }
+
+            if(clicouEm == "Weekly"){
+                let horaInicio = document.querySelectorAll('.hora-inicio')
+                for(let i = 0; i < valores[1].card.length; i++ ){
+                    let novoValor = valores[1].card[i].valor
+                    horaInicio[i].textContent = novoValor
+                    }
+            }
+
+            if(clicouEm == "Monthly"){
+                let horaInicio = document.querySelectorAll('.hora-inicio')
+                for(let i = 0; i < valores[2].card.length; i++ ){
+                    let novoValor = valores[2].card[i].valor
+                    horaInicio[i].textContent = novoValor
+                    }
+            }
+        });
+    });
+}
+funcaoClick();
+
+// console.log(valores[1].card[3])
