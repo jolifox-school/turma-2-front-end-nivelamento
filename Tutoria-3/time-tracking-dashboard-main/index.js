a653507ee4e782f5bb601f987145d21abe4b0459
Rafael <rafaelbrunodasilva@yahoo.com.br>
2023-04-03 19:40:19 -0300

    T2-32-desafio-front-end resposicionando arquivos index html e js e CSS

--- Tutoria-3/time-tracking-dashboard-main/index.js
+++ Tutoria-3/time-tracking-dashboard-main/index.js
@@ -1,9 +1,11 @@
+"use strict";
+
 /* 
 *  1 -  Usar a variável "valores" para inserir os dados nos cards, em cada período somar os valores de 
 *       cada categoria.
 *  2 - Adicionar o evento "click" no menu, onde ao clicar as informações nos cards mudem de acordo com 
 *       o período escolhido
-*/      
+*/
 
 const valores = [
     {
@@ -94,3 +96,51 @@ const valores = [
         ]
     },
 ]
+
+const menuItems = document.querySelectorAll(".menu-items li");
+
+menuItems.forEach((e) => {
+    // console.log(e.innerHTML);
+    e.addEventListener("click", updateCards, false);
+});
+
+function updateCards() {
+    menuItems.forEach((e) => {
+        e.classList.remove("active");
+    });
+
+    const horaInicioElements = document.querySelectorAll(".hora-inicio");
+    let sum = 0;
+
+    this.classList.add("active");
+    if (this.innerHTML == "Daily") {
+        horaInicioElements.forEach((horaInicioElement, index) => {
+            horaInicioElement.innerHTML = valores[0].card[index].valor + "hrs";
+        });
+
+        valores[0].card.forEach((e) => {
+            sum += e.valor;
+        });
+        console.log(sum);
+    }
+    else if (this.innerHTML == "Weekly") {
+        horaInicioElements.forEach((horaInicioElement, index) => {
+            horaInicioElement.innerHTML = valores[1].card[index].valor + "hrs";
+        });
+
+        valores[1].card.forEach((e) => {
+            sum += e.valor;
+        });
+        console.log(sum);
+    }
+    else {
+        horaInicioElements.forEach((horaInicioElement, index) => {
+            horaInicioElement.innerHTML = valores[2].card[index].valor + "hrs";
+        });
+
+        valores[2].card.forEach((e) => {
+            sum += e.valor;
+        });
+        console.log(sum);
+    }
+}
